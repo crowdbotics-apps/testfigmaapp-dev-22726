da5b474b6c912ec4e67b0b0eb86299c1f65278d8
Crowdbotics <team@crowdbotics.com>
2021-07-07 20:30:13 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/testfigmaapp_dev_22726/MainActivity.java
@@ -0,0 +1,15 @@
+package com.testfigmaapp_dev_22726;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "testfigmaapp_dev_22726";
+  }
+}
